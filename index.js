a25e825908f23a625a877411ee01f8e12188e1fe
DmitriySavin <debilv556@gmail.com>
2025-06-15 00:42:38 +0300

    add

---
+++ index.js
@@ -0,0 +1,22 @@
+const refsMainBtn = document.querySelector('.main-btn');
+const refsMainText = document.querySelector('.main-text');
+
+refsMainBtn.textContent = refsMainText.textContent;
+
+// 
+
+const refsMainImg = document.querySelector('.main-image');
+refsMainImg.src = './images/pexels-saizstudio-21701019.jpg';
+
+// 
+
+const refsMainLink = document.querySelector('.main-link');
+const refsSecondImg = document.querySelector('.secong-img');
+
+refsMainLink.href = '/images/pexels-saizstudio-21701019.jpg';
+refsSecondImg.alt = 'Отличное фото!';
+
+//
+
+const refsListItem = document.querySelector('.list-item');
+refsListItem.textContent = 'Abvgd';
